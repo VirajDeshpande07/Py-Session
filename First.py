8aca6d31eb00b32e3b922ad2aee7c76b58cab1b3
VirajDeshpande07 <vdeshpande812@gmail.com>
2026-09-10 15:39:34 +0530

    commit changes

--- First.py
+++ First.py
@@ -4,7 +4,7 @@ result = n%2 == 0
 print("Number is odd :", not result)
 
 #2.write a program to print age in days
-output: 3 years = 1095 days
+# output: 3 years = 1095 days
 age = int(input("Enter your age in years: "))
 days = age * 365
 print(f"{age} years = {days} days")
