a8aa71dd89e31cd027cdddb616de6f7f87ae2179
VirajDeshpande07 <vdeshpande812@gmail.com>
2026-09-10 15:30:24 +0530

    commit changes

---
+++ First.py
@@ -0,0 +1,17 @@
+#write a program to check the number is even or odd
+# n = int(input("enter a number: "))
+# result = n%2 == 0
+# print("Number is odd :", not result)
+
+#write a program to print age in days
+#output: 3 years = 1095 days
+# age = int(input("Enter your age in years: "))
+# days = age * 365
+# print(f"{age} years = {days} days")
+
+#write a program to convertt minutes into hours and print it
+#example : 135 is 2 hours 15 mintues
+mins = int(input("Enter mins: "))
+hours = mins // 60
+rem_mins = mins%60
+print(f"{mins} is {hours} hours and {rem_mins} minutes")
