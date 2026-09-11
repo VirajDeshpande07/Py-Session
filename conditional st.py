912ff8272699df2f12279c9f86cc1ebd6a7687b3
VirajDeshpande07 <vdeshpande812@gmail.com>
2026-09-11 15:34:21 +0530

    commit

---
+++ conditional st.py
@@ -0,0 +1,28 @@
+# age = int(input("Enter your age: "))
+# if age >=18:
+#     print("You are eligible to drive")
+# else:
+#     print("You are not eligible to drive")
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("Num is even")
+# else:
+#     print("Num is odd")
+
+#print days of the week according to the numbers.
+days = int(input("Enter numbers 1 to 7: "))
+if days==1:
+    print("Monday")
+elif days==2:
+    print("Tuesday")
+elif days==3:
+    print("Wednesday")
+elif days==4:
+    print("Thursday")
+elif days==5:
+    print("Friday")
+elif days==6:
+    print("Saturday")
+elif days==7:
+    print("Sunday")
